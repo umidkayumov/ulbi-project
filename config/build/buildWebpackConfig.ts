ebff7465c261d10622219e29b71338c17b34df75
Umid <umidjonkayumov2000@gmail.com>
2024-01-27 16:52:11 +0500

    decomposition of webpack config

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,27 @@
+import webpack from "webpack";
+import { buildLoaders } from "./buildLoaders";
+import { buildPlugins } from "./buildPlugin";
+import { buildResolves } from "./buildResolves";
+import { BuildOptions } from "./types/config";
+
+
+
+export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
+    const {paths, mode} = options;
+
+
+    return {
+        mode,
+        entry: paths.entry,
+        output: {
+            filename: "[name].[contenthash].js",
+            path: paths.build,
+            clean: true
+        },
+        plugins: buildPlugins(options),
+        module: {
+            rules: buildLoaders()
+        },
+        resolve: buildResolves(),
+    }
+}

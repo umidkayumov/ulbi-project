0cb95061689a7714503c57d420147e7c97911eea
Umid <umidjonkayumov2000@gmail.com>
2024-01-29 15:25:46 +0500

    add helper classNames

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
-import { Suspense, useContext, useState } from "react";
+import { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import { Link } from "react-router-dom";
 
-import "./styles/index.scss"
+import "./styles/index.scss";
+import { useTheme } from "./theme/useTheme";
+import { classNames } from "./helpers/classNames";
 
 import MainPage from "./pages/MainPage";
 import AboutPage from "./pages/AboutPage";
-import { Theme, ThemeContext } from "./theme/ThemeContext";
-import { useTheme } from "./theme/useTheme";
-
-
 
 
 const App = () => {
    const {theme, toggleTheme} = useTheme();
     return (
-        <div className={`App ${theme}`}>
+        <div className={classNames("App", {}, [theme])}>
             <button onClick={toggleTheme}>theme</button>
             <Link to="/">Main</Link>
             <Link to="/about">About</Link>
